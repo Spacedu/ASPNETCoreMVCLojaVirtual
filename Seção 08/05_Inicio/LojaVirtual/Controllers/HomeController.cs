fc344dd1143a9532d966406a670dcd141257d96f
Elias Costa <elias.ribeiro.s@gmail.com>
2019-01-28 20:39:12 -0200

    Enviando Cap 6-7-8

---
+++ Seção 08/05_Inicio/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ Seção 08/08_Fim/LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,30 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            /*
+             * SMTP -> Servidor que vai enviar a mensagem.
+             */
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("elias.ribeiro.s@gmail.com", "");
+            smtp.EnableSsl = true;
+
+            /*
+             * MailMessage -> Construir a mensagem
+             */
+            MailMessage mensagem = new MailMessage();
+
+        }
+    }
+}

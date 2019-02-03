ddfad9aa9e0d981df63a3a3bad0f6575705897d8
Elias Costa <elias.ribeiro.s@gmail.com>
2019-02-03 13:16:29 -0200

    Enviando Cap 09

---
+++ Seção 09/08_Fim/LojaVirtual/Database/LojaVirtualContext.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Database
+{
+    public class LojaVirtualContext : DbContext
+    {
+        /*
+         * EF Core - ORM
+         * ORM -> Bibliteca mapear Objetos para Banco de Dados Relacionais
+         */
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<NewsletterEmail> NewsletterEmails { get; set; }
+
+    }
+}
---
+++ Seção 09/08_Inicio/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var news = new NewsletterEmail() { Email = "ribeiro@gmail.com" };
+            return View(news);
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Opps! Tivemos um erro, tente novamente mais tarde!";
+
+                //TODO - Implementar Log
+            }
+            
+
+            return View("Contato");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
